f95dc6389054c9920be0e4febe7a3807725bcb5b
t-kelly <thomas.kelly@shopify.com>
2017-11-09 13:48:40 -0500

    Migrate project from shopify/slate

---
+++ src/assets/scripts/theme.js
@@ -0,0 +1,48 @@
+import '../styles/theme.scss';
+
+import $ from 'jquery';
+import {pageLinkFocus} from '@shopify/theme-a11y';
+import {cookiesEnabled} from '@shopify/theme-cart';
+import {wrapTable, wrapIframe} from '@shopify/theme-rte';
+
+window.slate = window.slate || {};
+window.theme = window.theme || {};
+
+/* = =============== Templates ================ */
+// import './templates/customers-addresses';
+// import './templates/customers-login.js';
+
+$(document).ready(() => {
+  // Common a11y fixes
+  pageLinkFocus($(window.location.hash));
+
+  $('.in-page-link').on('click', evt => {
+    pageLinkFocus($(evt.currentTarget.hash));
+  });
+
+  // Target tables to make them scrollable
+  const tableSelectors = '.rte table';
+
+  wrapTable({
+    $tables: $(tableSelectors),
+    tableWrapperClass: 'rte__table-wrapper',
+  });
+
+  // Target iframes to make them responsive
+  const iframeSelectors =
+    '.rte iframe[src*="youtube.com/embed"],' +
+    '.rte iframe[src*="player.vimeo"]';
+
+  wrapIframe({
+    $iframes: $(iframeSelectors),
+    iframeWrapperClass: 'rte__video-wrapper',
+  });
+
+  // Apply a specific class to the html element for browser support of cookies.
+  if (cookiesEnabled()) {
+    document.documentElement.className = document.documentElement.className.replace(
+      'supports-no-cookies',
+      'supports-cookies'
+    );
+  }
+});
