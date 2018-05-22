0c2640ebfc1c59692086e67b87ac42c0c73350d4
imontalvomiguel <ubaldo.im@gmail.com>
2018-05-22 15:14:48 -0500

    Add index template scripts file

---
+++ src/assets/scripts/templates/index.js
@@ -0,0 +1,10 @@
+import '../sections/product';
+
+import $ from 'jquery';
+import sections from '@shopify/theme-sections';
+
+$(document).ready(() => {
+  sections.load([
+    'product',
+  ]);
+});
